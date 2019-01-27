d0498ecbddbf87ffd14edd289fcef6ca2b9ea877
Jon <19751518+JonChica@users.noreply.github.com>
2019-01-27 12:15:18 +0100

    react excercise fix quotes

--- react-excercise/src/app.tsx
+++ react-excercise/src/app.tsx
@@ -1,19 +1,17 @@
-import * as React from 'react';
-import {MembersTableComponent} from './components';
+import * as React from "react";
+import { MembersTableComponent } from "./components";
 
-interface Props {
-}
+interface Props {}
 
 export class App extends React.Component<Props> {
   constructor(props: Props) {
     super(props);
-
   }
 
   public render() {
     return (
       <>
-        <MembersTableComponent/>
+        <MembersTableComponent />
       </>
     );
   }
